99e1619120f0912e99a31f579757476d34ac9f49
Joris Borgdorff <joris@thehyve.nl>
2016-12-08 17:35:20 +0100

    Tested utility function

--- src/test/java/org/radarcns/util/UtilityTest.java
+++ src/test/java/org/radarcns/util/UtilityTest.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.TreeMap;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
 
 public class UtilityTest {
@@ -64,5 +66,49 @@ public class UtilityTest {
         s.put("start", 1000L);
         s.put("end", 2000L);
         assertEquals("myUser-mySource-1000-2000", Utility.intervalKeyToMongoKey(s));
+    }
+
+    @Test
+    public void testParseArrayConfig() {
+        Map<String, String> testMap = new TreeMap<>();
+        testMap.put("testValues", "prop2,prop3");
+        testMap.put("prop1", "something1");
+        testMap.put("prop2", "something2");
+        testMap.put("prop3", "something3");
+        Map<String, String> result = Utility.parseArrayConfig(testMap, "testValues");
+        assertThat(result, hasEntry("prop2", "something2"));
+        assertThat(result, hasEntry("prop3", "something3"));
+        assertThat(result, not(hasKey("prop1")));
+        assertThat(result, not(hasKey("testValues")));
+    }
+
+    @Test
+    public void testNonExistingParseArrayConfig() {
+        Map<String, String> testMap = new TreeMap<>();
+        testMap.put("testValues", "prop2,prop3,prop4");
+        testMap.put("prop1", "something1");
+        testMap.put("prop2", "something2");
+        testMap.put("prop3", "something3");
+        assertEquals(null, Utility.parseArrayConfig(testMap, "testValues"));
+    }
+
+    @Test
+    public void testEmptyPropertyParseArrayConfig() {
+        Map<String, String> testMap = new TreeMap<>();
+        testMap.put("testValues", "prop2,prop3");
+        testMap.put("prop1", "something1");
+        testMap.put("prop2", "something2");
+        testMap.put("prop3", "");
+        assertEquals(null, Utility.parseArrayConfig(testMap, "testValues"));
+    }
+
+    @Test
+    public void testEmptyValueParseArrayConfig() {
+        Map<String, String> testMap = new TreeMap<>();
+        testMap.put("testValues", "");
+        testMap.put("prop1", "something1");
+        testMap.put("prop2", "something2");
+        testMap.put("prop3", "something3");
+        assertThat(Utility.parseArrayConfig(testMap, "testValues"), is(Collections.emptyMap()));
     }
 }
