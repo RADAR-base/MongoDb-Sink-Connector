34e116bd587f924c01b985eee4fe4a607d1c25b6
Joris Borgdorff <joris@thehyve.nl>
2016-12-14 14:42:10 +0100

    Added more license headers

--- src/test/java/org/radarcns/util/ValidClassTest.java
+++ src/test/java/org/radarcns/util/ValidClassTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.radarcns.util;
 
 import org.apache.kafka.common.config.ConfigDef;
