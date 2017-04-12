f3b3f4513c3e669ff05f4c1e468caa1b682608cc
Joris Borgdorff <joris@thehyve.nl>
2017-04-12 08:52:49 +0200

    Fixed copyright
    This reverts commit 336697ba49a07e4a38bacca3c4da1977cfdf99a0, reversing
    changes made to d73dbcb33076ba3f46d2302e523eaec7ea2c8037.

--- src/main/java/org/radarcns/serialization/RecordConverter.java
+++ src/main/java/org/radarcns/serialization/RecordConverter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2016 Kings College London and The Hyve
+ * Copyright 2017 The Hyve and King's College London
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
