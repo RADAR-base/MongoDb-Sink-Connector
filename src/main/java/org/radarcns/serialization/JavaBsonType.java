10bea48b4bfcb5fd2e20d11520809190cbe418cd
Joris Borgdorff <joris@thehyve.nl>
2018-01-10 14:04:56 +0100

    Removed unnecessary check

--- src/main/java/org/radarcns/serialization/JavaBsonType.java
+++ src/main/java/org/radarcns/serialization/JavaBsonType.java
@@ -68,7 +68,7 @@ public enum JavaBsonType {
             for (Map.Entry<?, ?> entry : map.entrySet()) {
                 String key = objectToString(entry.getKey());
                 Object value = entry.getValue();
-                if (key.startsWith("time")) {
+                if (key.startsWith("time") || key.equals("start") || key.equals("end")) {
                     BsonDateTime dateTime = toDateTime(value);
                     if (dateTime != null) {
                         doc.put(key, dateTime);
